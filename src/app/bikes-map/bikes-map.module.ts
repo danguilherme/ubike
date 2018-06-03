67a772971b9c324b71e208d283262b2352094fcd
Guilherme Ventura <guilhermeventura2@gmail.com>
2018-06-02 22:04:53 -0300

    Adiciona mapa na parada
    - Adiciona CSS básico

--- src/app/bikes-map/bikes-map.module.ts
+++ src/app/bikes-map/bikes-map.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AgmCoreModule } from '@agm/core';
 
 import { BikesMapRoutingModule } from './bikes-map-routing.module';
 import { MapComponent } from './map/map.component';
 
+const mapsApiKey = 'AIzaSyD6VNzmwUhhCw-Nwq3Q_YBg4wZ8rvVgc6Q';
+
 @NgModule({
   imports: [
     CommonModule,
-    BikesMapRoutingModule
+    BikesMapRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: mapsApiKey,
+    }),
   ],
-  declarations: [MapComponent]
+  declarations: [MapComponent],
 })
-export class BikesMapModule { }
+export class BikesMapModule {}
